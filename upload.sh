fd75544db9b30a0db43dbd2033f2046dddaca93b
ChoiseU <csw980928@gmail.com>
2025-01-24 17:36:05 +0900

    feat: upload bash 추가

---
+++ upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MESSAGE=$1
+# public upload
+hugo -t .
+cd public
+git add .
+git commit -m "$MESSAGE"
+git push
+
+# blog upload
+cd ..
+git add .
+git commit -m "$MESSAGE"
+git push
